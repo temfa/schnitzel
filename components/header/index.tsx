d2447a78ba097f13fa9c7e3d195cabd0830e6bb6
temfa <akinfetemitope5@gmail.com>
2025-02-10 14:04:03 +0100

    Almost Done

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -4,13 +4,14 @@ import styles from "./styles.module.css";
 import Link from "next/link";
 import Close from "@/svgs/close";
 import Bars from "@/svgs/bars";
+import Image from "next/image";
 
 export const Header = () => {
   const [mobile, setMobile] = useState(false);
   return (
     <div className={mobile ? `${styles.mobile} ${styles.header}` : styles.header}>
       <div className={styles.logo}>
-        <h2>Das Schnitzelparadies</h2>
+        <Image src="/images/logo.png" width={300} height={80} alt="Logo" />
         {mobile ? <Close action={() => setMobile(false)} /> : <Bars action={() => setMobile(true)} />}
       </div>
       <div className={styles.navbar}>
